3bf3b69246de334cd4fbd58ac0db9c4e94e89528
Afropenguin <mbrooks8@ucmerced.edu>
2021-01-27 15:20:49 -0800

    initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import re
+import time
+from collections import defaultdict
+from datetime import datetime
+
+import praw
+import requests
+from praw.models import MoreComments
+
+from redditParser import RedditParser
+import passwords
+
+reddit = praw.Reddit(
+    client_id=passwords.client_id,
+    client_secret=passwords.client_secret,
+    user_agent=passwords.user_agent
+)
+rparser = RedditParser(reddit)
+
+subreddits = ["stocks", "SPACs", "wallstreetbets", "options"]
+rparser.getRedditStockData(subreddits, printMe="all", limit=10000, time_filter="day")
+
+
---
+++ redditParser.py
@@ -0,0 +1,89 @@
+import requests
+import re
+import praw
+from collections import defaultdict
+from collections import Counter
+from praw.models import MoreComments
+import time
+from datetime import datetime
+
+
+class RedditParser:
+    def __init__(self, reddit):
+        self.tickerDict = self.getTickerDict()
+        self.reddit = reddit
+
+    def getTickerDict(self):
+        tickerDict = {}
+        with open("tickerSymbols.csv", "r") as f:
+            for line in f:
+                tickerDict[line.replace("\n", "")] = 0
+
+        with open("NYSE.txt", "r") as f:
+            for line in f:
+                tickerDict[line.split("	")[0]] = 0
+        return tickerDict
+
+    def getTickerSymbols(self, stuff, tickerDict):
+        tickers = re.findall(r"\b[A-Z][A-Z]+\b", stuff)
+        tempList = []
+        for ticker in tickers:
+            if ticker in tickerDict:
+                tempList.append(ticker)
+        return tempList
+
+    def removeBadSymbols(self, myDict, filter=0):
+        tempList = []
+        for thing in myDict:
+            if myDict[thing] < filter:
+                tempList.append(thing)
+
+        badSymbols = ["CEO", "DD", "FOR", "DO", "HAS", "TO", "RSI", "ETF", "EV", "USA", "EDIT", "AI", "AM", "PM",
+                      "TIME", "HI", "PPP", "LINK", "EOD", "ARE", "IM", "ALL", "IF", "AT",
+                      "CA", "IT", "IRS", "ROTH", "MOST", "HUGE", "MY", "HERO", "MORE", "FULL", "IMO", "ONE"]
+        for thing in tempList + badSymbols:
+            myDict.pop(thing, None)
+        return myDict
+
+    def getTickerSymbolsFromPost(self, submission):
+        tickerSymbols = []
+        tickerSymbols += self.getTickerSymbols(submission.title, self.tickerDict)
+        # Post Text
+        tickerSymbols += self.getTickerSymbols(submission.selftext, self.tickerDict)
+        # Post comments
+        for top_level_comment in submission.comments:
+            if isinstance(top_level_comment, MoreComments):
+                continue
+            tickerSymbols += self.getTickerSymbols(top_level_comment.body, self.tickerDict)
+        return tickerSymbols
+
+    def getRedditStockData(self, subreddit, printMe="all", minOccurances=2, time_filter="week", searchQuery="DD", limit=100):
+        for sub in subreddit:
+            tickers = defaultdict(int)
+            if printMe:
+                print("Summary for: %s" % sub)
+                print("Search Query:", searchQuery)
+                print("Minimum Occurances:", minOccurances)
+                print("Time Filter:", time_filter)
+
+            for submission in self.reddit.subreddit(sub).search(searchQuery, time_filter=time_filter):
+                if "?" not in submission.title and len(submission.selftext) > 200:
+                    if printMe == "all":
+                        try:
+                            print("Title:", submission.title, datetime.fromtimestamp(submission.created))
+                            print("Length:", len(submission.selftext))
+                            print("Link:", submission.url)
+                        except Exception as e:
+                            print(e)
+                    symbols = self.getTickerSymbolsFromPost(submission)
+                    if printMe == "all":
+                        print("Symbols in this post:", symbols, "\n")
+                    for ticker in symbols:
+                        tickers[ticker] += 1
+
+            self.removeBadSymbols(tickers, filter=minOccurances)
+            if printMe:
+                print("Ticker summary for: %s" % sub)
+                print(Counter(tickers).most_common(limit))
+                # print({k: v for k, v in sorted(tickers.items(), key=lambda item: item[1], reverse=True)})
+                print("\n")
